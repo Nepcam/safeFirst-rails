f14f198b16d29b519a48c5d714593875675c1972
Julie Kwok <julie.kwok@fluxfederation.com>
2018-11-22 17:12:02 +1300

    Removes typo in migrate file

--- db/migrate/20181121224139_create_jwt_blacklist.rb
+++ db/migrate/20181121224139_create_jwt_blacklist.rb
@@ -3,7 +3,7 @@ class CreateJwtBlacklist < ActiveRecord::Migration[5.2]
     create_table :jwt_blacklist do |t|
       t.string :jti, null: false
       t.datetime :exp, null: false
-    endUser
+    end
 
     add_index :jwt_blacklist, :jti
   end
