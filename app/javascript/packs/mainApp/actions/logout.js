96b772427678635b5eb8e44d7a6bd7c846e8a8ef
Julie Kwok <julie.kwok@fluxfederation.com>
2018-11-23 15:29:05 +1300

    Connects logout action with API and fixes actions

--- app/javascript/packs/mainApp/actions/logout.js
+++ app/javascript/packs/mainApp/actions/logout.js
@@ -1,27 +1,36 @@
-import { removeUser } from '../utils/auth';
+import { removeUserToken } from '../utils/auth';
+import request from '../utils/api';
 
 function requestLogout() {
   return {
-    type: 'LOGOUT_REQUEST',
-    isFetching: true,
-    isAuthenticated: true
+    type: 'LOGOUT_REQUEST'
   }
 }
 
-function receiveLogout() {
+function logoutSuccess() {
   return {
-    type: 'LOGOUT_SUCCESS',
-    isFetching: false,
-    isAuthenticated: false
+    type: 'LOGOUT_SUCCESS'
   }
 }
 
-// Logs the user out
+function logoutError() {
+  return {
+    type: 'LOGOUT_ERROR'
+  }
+}
+
 export function logoutUser() {
   return dispatch => {
-    document.location = "/#/";
     dispatch(requestLogout());
-    removeUser();
-    dispatch(receiveLogout());
+    return request('delete', '/users/sign_out')
+      .end((err, res) => {
+        if (res.status === 204) {
+          removeUserToken();
+          document.location = "/#";
+          dispatch(logoutSuccess());
+        } else {
+          dispatch(logoutError(`${err.message}: ${err.response.body.error}`));
+        }
+      });
   }
 }
