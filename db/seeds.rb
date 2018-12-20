095593b64a83b5b693b8c4e6eb810fd764ade3e1
Julie Kwok <julie.kwok@fluxfederation.com>
2018-12-21 10:49:14 +1300

    Fixes formatting in seeds file and ties hazards to a generic site

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-require 'csv'    
-require 'byebug'
+require 'csv'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -11,7 +10,7 @@ require 'byebug'
 
 # HazardCategories
 csv_text = File.read(Rails.root.join('db', 'seed_data', 'HazardCategories.csv'))
-csv = CSV.parse(csv_text, :headers => true)
+csv      = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   HazardCategory.find_or_create_by!(row.to_hash)
 end
@@ -23,20 +22,22 @@ HazardControlMethod.find_or_create_by!(name: 'Minimise')
 
 #Hazards
 csv_text = File.read(Rails.root.join('db', 'seed_data', 'Hazards.csv'))
-csv = CSV.parse(csv_text, :headers => true)
+csv      = CSV.parse(csv_text, :headers => true)
+site     = Site.create(name: 'Initial', location: 'Wellington, New Zealand')
+
 csv.each do |row|
-    next if Hazard.exists?(reference_number: row['reference_number'])
+  next if Hazard.exists?(reference_number: row['reference_number'])
 
-    hazard = Hazard.new(row.to_hash.slice('name','reference_number','control','risk'))
+  hazard = Hazard.new(row.to_hash.slice('name', 'reference_number', 'control', 'risk'))
+  hazard.site = site
 
-    hazard_category = HazardCategory.find_by(name: row['category'])
-    hazard_control_method = HazardControlMethod.find_by(name: row['hazard_control_method'])
+  hazard_category       = HazardCategory.find_by(name: row['category'])
+  hazard_control_method = HazardControlMethod.find_by(name: row['hazard_control_method'])
 
-    puts row if hazard_control_method == nil
+  puts row if hazard_control_method == nil
 
-    hazard.hazard_control_method = hazard_control_method
-    hazard.hazard_category = hazard_category
+  hazard.hazard_control_method = hazard_control_method
+  hazard.hazard_category       = hazard_category
 
-    hazard.save! 
-
+  hazard.save!
 end
